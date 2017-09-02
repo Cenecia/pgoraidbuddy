6d7f97992007b1ad343d1333b5df9f05ec675295
Chris <deviler@gmail.com>
2017-09-02 14:50:07 -0400

    added keys file

---
+++ app/controllers/keys.php
@@ -0,0 +1,8 @@
+<?php
+	define("ROOT_DIR", "/pgoraidhelper");
+	define("DB_HOST", 'localhost');
+	define("DB_DB", 'pgoraidbuddy');
+	define("DB_USER", 'root');
+	define("DB_PASS", '');
+	define("SITE_URL","http://localhost");
+	define("HOURS_DIFF",7);
