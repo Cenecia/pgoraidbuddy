48710e22a365e1d781bc1737c7611563f83d7c2b
Chris <deviler@gmail.com>
2017-09-03 17:08:34 -0400

    fixed layout issue for iphone

--- app/views/raid/create.php
+++ app/views/raid/create.php
@@ -25,68 +25,70 @@
 	?>
 	<form method="post" action="">
 		<div class="card card-inverse" style="background-color: #222224; border-color: #222224;">
-			<div class="col-12">
-				<fieldset>
-					<div class="form-group">
-						<label for="location">Gym Name</label>
-						<input type="text" name="location" id="location" class="form-control" required/>
-					</div>
-					<div class="form-group">
-						<label for="location">Google Map Search (optional)</label>
-						<input type="text" name="gmap" id="gmap" class="form-control" />
-					</div>
-					<div class="form-group">
-						<label for="expires">Expires in</label>
-						<div class="row">
-							<div class="col-4 col-sm-3">
-								<select id="hour" name="hour" class="form-control" required />
-									<option value="1">1</option>
-									<option value="0">0</option>
-								</select>
-								<small id="hourText" class="form-text text-center">hours</small>
-							</div>
-							<div class="col-1 col-sm-3">
-								:
-							</div>
-							<div class="col-4">
-								<select name="minute" id="minute" class="form-control">
-								<?php
-									for ($x = 0; $x <= 59; $x++) {
-										echo "<option value='$x'>".sprintf("%02d",$x)."</option>";
-									}
-								?>
-								</select>
-								<small id="minuteText" class="form-text text-center">minutes</small>
-							</div>
+			<div class="card-block">
+				<div class="col-12">
+					<fieldset>
+						<div class="form-group">
+							<label for="location">Gym Name</label>
+							<input type="text" name="location" id="location" class="form-control" required/>
 						</div>
-					</div>
-					<div class="form-group">
-						<label for="pokemon">Pokémon</label>
-						<input type="hidden" name="pokemon" id="pokemon" required/>
-						<div>
-							<?php
-								for($tier = 5; $tier > 0; $tier--){
-							?>
-								<p class="lead">
+						<div class="form-group">
+							<label for="location">Google Map Search (optional)</label>
+							<input type="text" name="gmap" id="gmap" class="form-control" />
+						</div>
+						<div class="form-group">
+							<label for="expires">Expires in</label>
+							<div class="row">
+								<div class="col-5 col-sm-3">
+									<select id="hour" name="hour" class="form-control" required />
+										<option value="1">1</option>
+										<option value="0">0</option>
+									</select>
+									<small id="hourText" class="form-text text-center">hours</small>
+								</div>
+								<div class="col-1 col-sm-3">
+									:
+								</div>
+								<div class="col-5 col-sm-3">
+									<select name="minute" id="minute" class="form-control">
 									<?php
-										for($star = 1; $star <= $tier; $star++){
-									?>
-										<i class="fa fa-star" aria-hidden="true"></i>
-									<?php
+										for ($x = 0; $x <= 59; $x++) {
+											echo "<option value='$x'>".sprintf("%02d",$x)."</option>";
 										}
 									?>
-								</p>
-							<?php
-									foreach($data['pokemon'][$tier] as $pokemon){
-							?>
-									<img src="../public/img/pokemon/<?=$pokemon->name?>.png" class="col-3 teamIcon" alt="<?=$pokemon->name?>" data-pokemonID="<?=$pokemon->id?>">
-							<?php
-									}
-							}
-							?>
+									</select>
+									<small id="minuteText" class="form-text text-center">minutes</small>
+								</div>
+							</div>
 						</div>
-					</div>
-				</fieldset>
+						<div class="form-group">
+							<label for="pokemon">Pokémon</label>
+							<input type="hidden" name="pokemon" id="pokemon" required/>
+							<div>
+								<?php
+									for($tier = 5; $tier > 0; $tier--){
+								?>
+									<p class="lead">
+										<?php
+											for($star = 1; $star <= $tier; $star++){
+										?>
+											<i class="fa fa-star" aria-hidden="true"></i>
+										<?php
+											}
+										?>
+									</p>
+								<?php
+										foreach($data['pokemon'][$tier] as $pokemon){
+								?>
+										<img src="../public/img/pokemon/<?=$pokemon->name?>.png" class="col-3 teamIcon" alt="<?=$pokemon->name?>" data-pokemonID="<?=$pokemon->id?>">
+								<?php
+										}
+								}
+								?>
+							</div>
+						</div>
+					</fieldset>
+				</div>
 			</div>
 		</div>
 		<br/>
